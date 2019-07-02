a85f986e3b8bab23ebc73d7ce4bf64a291e194a3
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-02 00:27:00 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -5,4 +5,9 @@ class Actor < ActiveRecord::Base
   def full_name
     "#{first_name} #{last_name}"
   end
+
+  def list_roles
+    "#{self.characters.first.name} - #{self.characters.first.show.name}"
+    
+  end
 end
