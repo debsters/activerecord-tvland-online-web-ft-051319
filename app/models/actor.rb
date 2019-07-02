9435b4899044dc531f683aeadc550a4737594f35
Deborah Barcenas <deborah.barcenas@gmail.com>
2019-07-02 00:32:29 +0000

    Automatically backed up by Learn

--- app/models/actor.rb
+++ app/models/actor.rb
@@ -7,6 +7,6 @@ class Actor < ActiveRecord::Base
   end
 
   def list_roles
-    "#{self.characters.first.name} - #{self.characters.first.show.name}"
+    self.characters.map{ |character| "#{character.name} - #{character.show.name}"}
   end
 end
